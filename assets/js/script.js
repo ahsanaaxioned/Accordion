936fe44624ea95d59c67359d0250b3e748404018
ahsanaaxioned <ahsana@axioned.com>
2022-10-13 18:34:23 +0530

    event added on li

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,15 @@
-/* Author: 
-
-*/
+// global variable decleration start here
+const faqItem = document.querySelectorAll(".faq-item");
+const answer = document.querySelectorAll(".answer");
+const close = document.querySelectorAll(".close");
+// global variable decleration start here
+// accordion function start here
+faqItem.forEach(function(ele,idx){
+    ele.addEventListener("click",function(){
+        ele.classList.toggle("active");
+    })
+})
+// accordion function start here
 
 
 
