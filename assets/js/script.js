3aa25122504778d3439ecff20bff143c537be608
ahsanaaxioned <ahsana@axioned.com>
2022-10-17 11:17:10 +0530

    js correction done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,21 @@
 // global variable decleration start here
 const faqItem = document.querySelectorAll(".faq-item");
-// global variable decleration start here
+// global variable decleration end here
 // accordion function start here
-faqItem.forEach(function (ele, idx) {
-    ele.addEventListener("click", function (e) {
-        for (let i=0; i<faqItem.length; i++) {
-            if(idx != i) faqItem[i].classList.remove("active");
-        }
-        e.currentTarget.classList.toggle("active");
-    });
-})
+if (faqItem) {
+    faqItem.forEach(function (ele, idx) {
+        ele.addEventListener("click", function () {
+            const active = document.querySelector(".active");
+            if (ele.classList.contains("active")) {
+                ele.classList.remove("active");
+            } else {
+                if (active) {
+                    active.classList.remove("active");
+                }
+                ele.classList.add("active");
+            }
+
+        })
+    })
+}
 // accordion function start here
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
